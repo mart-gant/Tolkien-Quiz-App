09a703a60e907390cba04d1bfa6be22375a67e9a
Jakub Hyla <jakub.hyla.external@scalable.capital>
2022-03-05 15:55:05 +0100

    Use basic props

--- presentation/src/main/java/com/martgant/tolkienquizapp/QuestionFragment.kt
+++ presentation/src/main/java/com/martgant/tolkienquizapp/QuestionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 import com.martgant.tolkienquizapp.databinding.FragmentQuestionBinding
 
@@ -24,13 +25,24 @@ class QuestionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.questionDescription.text = context?.getString(R.string.question_1)
-        binding.answer1.text = context?.getString(R.string.question_1_answer_correct)
-        binding.answer2.text = context?.getString(R.string.question_1_answer_incorrect_1)
+        val props = QuestionProps(
+            questionRes = R.string.question_1,
+            answer1Res = R.string.question_1_answer_correct,
+            answer2Res = R.string.question_1_answer_incorrect_1
+        )
+        binding.questionDescription.text = context?.getString(props.questionRes)
+        binding.answer1.text = context?.getString(props.answer1Res)
+        binding.answer2.text = context?.getString(props.answer2Res)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private data class QuestionProps(
+        @StringRes val questionRes: Int,
+        @StringRes val answer1Res: Int,
+        @StringRes val answer2Res: Int
+    )
 }
