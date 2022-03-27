37b8ac998207be8efba7439e8ee8170a9740a066
mart-gant <martgant@gmail.com>
2022-03-27 18:01:43 +0200

    Update BaseFragment.kt

--- presentation/src/main/java/com/martgant/tolkienquizapp/base/BaseFragment.kt
+++ presentation/src/main/java/com/martgant/tolkienquizapp/base/BaseFragment.kt
@@ -5,15 +5,13 @@ import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
-
-    protected fun showToast(@StringRes textRes: Int, duration: Int = Toast.LENGTH_LONG) {
-        context?.let {
+    protected fun showToast(toastTextRes: Int) {
+        context?.let{
             Toast.makeText(it, textRes, duration).show()
         }
     }
-
     @StringRes
-    protected fun getStringByName(stringName: String): Int {
+    protected fun getStringByName(StringName: String): Int{
         return resources.getIdentifier(stringName, "string", activity?.packageName)
     }
 }
