8a5edf787ef5d556d82a6eda1c070f261cd60ca7
mart-gant <martgant@gmail.com>
2022-03-27 18:00:55 +0200

    Update QuestionFragment.kt

--- presentation/src/main/java/com/martgant/tolkienquizapp/QuestionFragment.kt
+++ presentation/src/main/java/com/martgant/tolkienquizapp/QuestionFragment.kt
@@ -36,58 +36,56 @@ class QuestionFragment : BaseFragment() {
         refresh()
     }
 
-    private fun refresh() {
-        val (questionRes, answers) = getQuestionProps()
-        binding.questionDescription.text = context?.getString(questionRes)
-        val buttons = listOf(binding.answer1, binding.answer2)
-        buttons.forEachIndexed { index, button ->
-            bindButton(button, answers[index])
-        }
-    }
+   private fun refresh(){
+       val(questionRes, answers) = getQuestionProps()
+       binding.questionDescription.text = context?.getString(questionRes)
+       val buttons = listOf(binding.answer1, binding.answer2)
+       buttons.forEachIndexed {index, button -> bindButton(button, answers[index])
+   }
+   }
+       private fun bindButton(button: Button, answer: QuestionProps.Answer){
+           button.text = context?.getSring(answer.answerRes)
+           button.setOnClickListener {
+               onAnswerSubmitted(answer.isCorrect)
+           }
 
-    private fun bindButton(button: Button, answer: QuestionProps.Answer) {
-        button.text = context?.getString(answer.answerRes)
-        button.setOnClickListener {
-            onAnswerSubmitted(answer.isCorrect)
-        }
-    }
-
-    // Now let's make the on answer callback more general
-    private fun onAnswerSubmitted(isCorrect: Boolean) {
-        val toastTextRes = when {
+       }
+    private fun onAnswerSubmitted(isCorrect: Boolean){
+        val toastTextRes = when{
             isCorrect -> R.string.correct_answer
             else -> R.string.incorrect_answer
         }
         showToast(toastTextRes)
         refresh()
     }
-
-    private fun getQuestionProps(): QuestionProps {
-        val questionNumber = Random.nextInt(1, 5)
-
-        val correctAnswer = QuestionProps.Answer(
-            answerRes = getStringByName(ANSWER_CORRECT_FORMAT.format(questionNumber)),
-            isCorrect = true
+    private fun getQuestionProps(): QuestionProps{
+        val questionNumber = Random.nextInt(1,5)
+        val correctAnswer = QuestionProps.Answer (
+            answerRes = getStringByName(ANSWER_CORRECT_FORMAT.format(questionNumber))
+         isCorrect =  true
         )
-
-        val incorrectAnswer = QuestionProps.Answer(
-            answerRes = getStringByName(ANSWER_INCORRECT_FORMAT.format(questionNumber, 1)),
-            isCorrect = false
+        val incorrectAnswer = QuestionProps.Answer (
+            answerRes = getStringByName(ANSWER_INCORRECT_FORMAT.format(questionNumber))
+                    isCorrect = false
         )
-
-        val questionRes = getStringByName(QUESTION_FORMAT.format(questionNumber))
+        val questionRes = getStringByName(ANSWER_CORRECT_FORMAT.format(questionNumber))
         val answers = listOf(correctAnswer, incorrectAnswer).shuffled()
-
         return QuestionProps(questionRes, answers)
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    private data class QuestionProps(@StringRes val questionRes: Int, val answers: List<Answer>) {
+    private data class QuestionProps(@StringRes val questionRes: Int, val answers: List<Answer>){
         data class Answer(@StringRes val answerRes: Int, val isCorrect: Boolean)
     }
 
+
+
 }
+
+
+
